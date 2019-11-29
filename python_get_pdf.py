708e5562e1845773c6d2c2106716f9f8b43c499f
Jevgeni Barkin <zekaxdpro@gmail.com>
2019-11-29 23:31:19 +0200

    Update python_get_pdf.py

--- python_get_pdf.py
+++ python_get_pdf.py
@@ -10,7 +10,7 @@ def get_pdf():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+    pdf.cell(200, 10, txt="Welcome to Python!!", ln=1, align="C")
     pdf.output("example1.pdf")
     webbrowser.open("example1.pdf")
 
